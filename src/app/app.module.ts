ab279d4d43100e874f04427fe53d16cc64b34b0a
alvaro-java <alvarito96azu@gmail.com>
2020-06-10 16:02:09 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AltausuarioComponent } from '../components/altausuario/altausuario.component';
 
 //Hola (Miguel)
 //HOLA SERGIO
 //Hola Nino
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltausuarioComponent
   ],
   imports: [
     BrowserModule,
