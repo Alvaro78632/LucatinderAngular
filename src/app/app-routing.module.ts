1104b816de13b65c6a7c247820394091e95c4bc0
Nino Casinelli <ninocasinelli@gmail.com>
2020-06-11 08:14:23 +0200

    Nino
    Arreglo del routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ListacontactoComponent } from '../components/listacontacto/listacontacto.component';
-
+import { ListapoblacionComponent} from '../components/listapoblacion/listapoblacion.component';
 
 const routes: Routes = [
   { 
     path: 'contactos', 
-    component: ListacontactoComponent 
-  }
-  
-import { ListapoblacionComponent} from '../components/listapoblacion/listapoblacion.component';
-
-
-const routes: Routes = [
-  { path: 'listapoblacion',
+    component: ListacontactoComponent
+  },
+  {
+    path: 'listapoblacion',
     component: ListapoblacionComponent
-  }
+ }
+ 
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
