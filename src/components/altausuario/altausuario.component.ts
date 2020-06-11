e6713996ed44e03b5d8058869c5db0c89e7ee8dd
alvaro-java <alvarito96azu@gmail.com>
2020-06-11 18:44:54 +0200

    porfin alta usuario, somos unos maquinas

--- src/components/altausuario/altausuario.component.ts
+++ src/components/altausuario/altausuario.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+  import { Component, OnInit } from '@angular/core';
+  import { Perfil} from '../../model/perfil';
+  import { PerfilService} from '../../service/perfil.service';
+  import { PoblacionService} from '../../service/poblacion.service';
+  import { Router } from '@angular/router';
+  import { Poblacion} from '../../model/poblacion';
 @Component({
   selector: 'app-altausuario',
   templateUrl: './altausuario.component.html',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltausuarioComponent implements OnInit {
 
-  constructor() { }
+  perfil:Perfil;
+  listaPoblacion:Poblacion[];
+
+  constructor(private perfilService: PerfilService,
+    private router: Router,private poblacionService:PoblacionService) { }
 
   ngOnInit(): void {
+    this.perfil = this.perfilService.emptyPerfil();
+    this.poblacionService.getPoblaciones()
+    .subscribe(data => {
+      console.log(data);
+      this.listaPoblacion=data;
+    });
+  }
+  
+  Addperfil():void{
+    console.log("linea 30 componente altausuario" + this.perfil);
+
+    this.perfilService.addPerfil(this.perfil)
+    .subscribe(data => {
+      console.log("Todo OK");
+    });
+    this.perfil = this.perfilService.emptyPerfil();
   }
 
 }
